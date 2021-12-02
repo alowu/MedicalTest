8c9ba4aeb030280420b4595e3fdd7650f72c5980
Ilia <ilya.bela@yandex.ru>
2021-12-03 00:37:07 +0300

    some refactor

---
+++ MedicalTest/Forms/GraphicsScreen.Designer.cs
@@ -0,0 +1,377 @@
+﻿
+namespace MedicalTest
+{
+	partial class GraphicsScreen
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Title title5 = new System.Windows.Forms.DataVisualization.Charting.Title();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label_surname = new System.Windows.Forms.Label();
+			this.label_name = new System.Windows.Forms.Label();
+			this.label_dad_name = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label_age = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label_sex = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label_date = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label_type = new System.Windows.Forms.Label();
+			this.label_time = new System.Windows.Forms.Label();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.button_exit = new System.Windows.Forms.Button();
+			this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.chart5 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart5)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(65, 17);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Пациент";
+			// 
+			// label_surname
+			// 
+			this.label_surname.AutoSize = true;
+			this.label_surname.Location = new System.Drawing.Point(187, 9);
+			this.label_surname.Name = "label_surname";
+			this.label_surname.Size = new System.Drawing.Size(70, 17);
+			this.label_surname.TabIndex = 1;
+			this.label_surname.Text = "Фамилия";
+			// 
+			// label_name
+			// 
+			this.label_name.AutoSize = true;
+			this.label_name.Location = new System.Drawing.Point(417, 9);
+			this.label_name.Name = "label_name";
+			this.label_name.Size = new System.Drawing.Size(35, 17);
+			this.label_name.TabIndex = 2;
+			this.label_name.Text = "Имя";
+			// 
+			// label_dad_name
+			// 
+			this.label_dad_name.AutoSize = true;
+			this.label_dad_name.Location = new System.Drawing.Point(647, 9);
+			this.label_dad_name.Name = "label_dad_name";
+			this.label_dad_name.Size = new System.Drawing.Size(71, 17);
+			this.label_dad_name.TabIndex = 3;
+			this.label_dad_name.Text = "Отчество";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 41);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(62, 17);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Возраст";
+			// 
+			// label_age
+			// 
+			this.label_age.AutoSize = true;
+			this.label_age.Location = new System.Drawing.Point(187, 41);
+			this.label_age.Name = "label_age";
+			this.label_age.Size = new System.Drawing.Size(62, 17);
+			this.label_age.TabIndex = 5;
+			this.label_age.Text = "Возраст";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 76);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(34, 17);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Пол";
+			// 
+			// label_sex
+			// 
+			this.label_sex.AutoSize = true;
+			this.label_sex.Location = new System.Drawing.Point(187, 76);
+			this.label_sex.Name = "label_sex";
+			this.label_sex.Size = new System.Drawing.Size(34, 17);
+			this.label_sex.TabIndex = 7;
+			this.label_sex.Text = "Пол";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 116);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(125, 17);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Дата назначения";
+			// 
+			// label_date
+			// 
+			this.label_date.AutoSize = true;
+			this.label_date.Location = new System.Drawing.Point(187, 116);
+			this.label_date.Name = "label_date";
+			this.label_date.Size = new System.Drawing.Size(42, 17);
+			this.label_date.TabIndex = 9;
+			this.label_date.Text = "Дата";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(417, 41);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(95, 17);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "Тип нагрузки";
+			// 
+			// label_type
+			// 
+			this.label_type.AutoSize = true;
+			this.label_type.Location = new System.Drawing.Point(647, 41);
+			this.label_type.Name = "label_type";
+			this.label_type.Size = new System.Drawing.Size(95, 17);
+			this.label_type.TabIndex = 11;
+			this.label_type.Text = "Тип нагрузки";
+			// 
+			// label_time
+			// 
+			this.label_time.AutoSize = true;
+			this.label_time.Location = new System.Drawing.Point(1037, 41);
+			this.label_time.Name = "label_time";
+			this.label_time.Size = new System.Drawing.Size(50, 17);
+			this.label_time.TabIndex = 12;
+			this.label_time.Text = "Время";
+			// 
+			// chart1
+			// 
+			chartArea1.AxisX.Title = "Время";
+			chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			this.chart1.Location = new System.Drawing.Point(25, 150);
+			this.chart1.Name = "chart1";
+			series1.BorderWidth = 3;
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series1.Color = System.Drawing.Color.Blue;
+			series1.Legend = "Legend1";
+			series1.Name = "Series1";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(1250, 600);
+			this.chart1.TabIndex = 13;
+			this.chart1.Text = "chart1";
+			title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			title1.Name = "Title1";
+			title1.Text = "Кровяное давление";
+			this.chart1.Titles.Add(title1);
+			// 
+			// button_exit
+			// 
+			this.button_exit.Location = new System.Drawing.Point(1179, 109);
+			this.button_exit.Name = "button_exit";
+			this.button_exit.Size = new System.Drawing.Size(75, 23);
+			this.button_exit.TabIndex = 18;
+			this.button_exit.Text = "Exit";
+			this.button_exit.UseVisualStyleBackColor = true;
+			this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+			// 
+			// chart2
+			// 
+			chartArea2.AxisX.Title = "Время";
+			chartArea2.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			chartArea2.Name = "ChartArea1";
+			this.chart2.ChartAreas.Add(chartArea2);
+			this.chart2.Location = new System.Drawing.Point(25, 800);
+			this.chart2.Name = "chart2";
+			series2.BorderWidth = 3;
+			series2.ChartArea = "ChartArea1";
+			series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series2.Color = System.Drawing.Color.Blue;
+			series2.Legend = "Legend1";
+			series2.Name = "Series1";
+			this.chart2.Series.Add(series2);
+			this.chart2.Size = new System.Drawing.Size(1250, 600);
+			this.chart2.TabIndex = 23;
+			this.chart2.Text = "chart2";
+			title2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			title2.Name = "Title1";
+			title2.Text = "Кровяное давление";
+			this.chart2.Titles.Add(title2);
+			// 
+			// chart3
+			// 
+			chartArea3.AxisX.Title = "Время";
+			chartArea3.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			chartArea3.Name = "ChartArea1";
+			this.chart3.ChartAreas.Add(chartArea3);
+			this.chart3.Location = new System.Drawing.Point(25, 1450);
+			this.chart3.Name = "chart3";
+			series3.BorderWidth = 3;
+			series3.ChartArea = "ChartArea1";
+			series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series3.Color = System.Drawing.Color.Blue;
+			series3.Legend = "Legend1";
+			series3.Name = "Series1";
+			this.chart3.Series.Add(series3);
+			this.chart3.Size = new System.Drawing.Size(1250, 600);
+			this.chart3.TabIndex = 24;
+			this.chart3.Text = "chart3";
+			title3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			title3.Name = "Title1";
+			title3.Text = "Кровяное давление";
+			this.chart3.Titles.Add(title3);
+			// 
+			// chart4
+			// 
+			chartArea4.AxisX.Title = "Время";
+			chartArea4.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			chartArea4.Name = "ChartArea1";
+			this.chart4.ChartAreas.Add(chartArea4);
+			this.chart4.Location = new System.Drawing.Point(25, 2100);
+			this.chart4.Name = "chart4";
+			series4.BorderWidth = 3;
+			series4.ChartArea = "ChartArea1";
+			series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series4.Color = System.Drawing.Color.Blue;
+			series4.Legend = "Legend1";
+			series4.Name = "Series1";
+			this.chart4.Series.Add(series4);
+			this.chart4.Size = new System.Drawing.Size(1250, 600);
+			this.chart4.TabIndex = 25;
+			this.chart4.Text = "chart4";
+			title4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			title4.Name = "Title1";
+			title4.Text = "Кровяное давление";
+			this.chart4.Titles.Add(title4);
+			// 
+			// chart5
+			// 
+			chartArea5.AxisX.Title = "Время";
+			chartArea5.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			chartArea5.Name = "ChartArea1";
+			this.chart5.ChartAreas.Add(chartArea5);
+			this.chart5.Location = new System.Drawing.Point(25, 2750);
+			this.chart5.Name = "chart5";
+			series5.BorderWidth = 3;
+			series5.ChartArea = "ChartArea1";
+			series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series5.Color = System.Drawing.Color.Blue;
+			series5.Legend = "Legend1";
+			series5.Name = "Series1";
+			this.chart5.Series.Add(series5);
+			this.chart5.Size = new System.Drawing.Size(1250, 600);
+			this.chart5.TabIndex = 26;
+			this.chart5.Text = "chart5";
+			title5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			title5.Name = "Title1";
+			title5.Text = "Кровяное давление";
+			this.chart5.Titles.Add(title5);
+			// 
+			// GraphicsScreen
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScroll = true;
+			this.ClientSize = new System.Drawing.Size(1322, 720);
+			this.Controls.Add(this.chart5);
+			this.Controls.Add(this.chart4);
+			this.Controls.Add(this.chart3);
+			this.Controls.Add(this.chart2);
+			this.Controls.Add(this.button_exit);
+			this.Controls.Add(this.chart1);
+			this.Controls.Add(this.label_time);
+			this.Controls.Add(this.label_type);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label_date);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label_sex);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label_age);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label_dad_name);
+			this.Controls.Add(this.label_name);
+			this.Controls.Add(this.label_surname);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "GraphicsScreen";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "  ";
+			this.Load += new System.EventHandler(this.GraphicsScreen_Load);
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart5)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label_surname;
+		private System.Windows.Forms.Label label_name;
+		private System.Windows.Forms.Label label_dad_name;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label_age;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label_sex;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label_date;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label_type;
+		private System.Windows.Forms.Label label_time;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.Button button_exit;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart5;
+	}
+}
---
+++ MedicalTest/Forms/TestScreen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedicalTest
+{
+	public partial class TestScreen : Form
+	{
+		public TestScreen()
+		{
+			InitializeComponent();
+		}
+
+		GraphicsScreen graphicsScreen = new GraphicsScreen();
+
+		private void graphicsSreen_FormClosed(object sender, FormClosedEventArgs e) 
+		{
+			this.Close();
+		}
+
+		private void button_start_Click(object sender, EventArgs e)
+		{
+			graphicsScreen.ShowDialog();
+		}
+
+		private void button_back_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+		bool flag = false;
+		private void button_setup_Click(object sender, EventArgs e)
+		{
+			flag = !flag;
+			pictureBox_css.Visible = flag;
+			pictureBox_presure.Visible = flag;
+			pictureBox_resist.Visible = flag;
+			pictureBox_temp.Visible = flag;
+			pictureBox_vlazh.Visible = flag;
+		}
+	}
+}
